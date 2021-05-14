7806d1768ddb6ea9fa0d8eb48b1dacea80239083
Richad MERALLI <richad.meralli@gmail.com>
2021-05-14 13:56:07 +0200

    added delete list method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: [:index ,:show, :create, :new] do
+  resources :lists, only: [:index ,:show, :create, :new, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
